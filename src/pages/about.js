1451fbcbf6cd7ebb078792c2964ce58b1da49796
Justin Green <jgreen@accruent.com>
2021-02-08 08:14:52 -0600

    update about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -15,8 +15,7 @@ const about = () => (
         The data was compiled by using python and the boardgamegeek API.
       </p>
       <p>
-        This website was created using <a href="https://gatsbyjs.com/" target="_blank" rel="noreferrer">gatsby</a> and 
-        hosted using <a href="https://surge.sh/" target="_blank" rel="noreferrer">surge</a>.
+        This website was created using <a href="https://gatsbyjs.com/" target="_blank" rel="noreferrer">gatsby</a>.
       </p>
       <p>
         To contact me regarding bugs, feature requests, or general comments, please message <a href="https://boardgamegeek.com/user/userid122002" target="_blank" rel="noreferrer">me</a> on boardgamegeek.
