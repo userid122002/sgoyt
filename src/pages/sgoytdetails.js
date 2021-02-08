5c959e391b005853c85484c0e7a7880d765dfc73
Justin Green <jgreen@accruent.com>
2021-02-08 15:39:33 -0600

    update detail page sorting

--- src/pages/sgoytdetails.js
+++ src/pages/sgoytdetails.js
@@ -34,7 +34,7 @@ export const query = graphql`
             }
             totalCount
         }
-        allSgoytCsv {
+        allSgoytCsv(sort: {fields: rownum, order: DESC}) {
             nodes {
                 game
                 gameid
