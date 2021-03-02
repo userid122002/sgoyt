990edbd28a159734e1153a21076a8404a5263b9a
Justin Green <jgreen@accruent.com>
2021-03-02 08:08:20 -0600

    ga

--- gatsby-config.js
+++ gatsby-config.js
@@ -43,6 +43,14 @@ module.exports = {
       },
     },
     `gatsby-plugin-sass`,
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        trackingIds: [
+          "UA-190898348-1",
+        ],
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     `gatsby-plugin-offline`,
